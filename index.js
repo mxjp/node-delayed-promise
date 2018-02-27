5951e02a22535ca9b704855ca20ba9906f636e7f
Peter Juras <me@peterjuras.com>
2018-02-27 18:46:11 +0100

    Use Promise.resolve to allow for undefined promises

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-'use strict';
+"use strict";
 
 module.exports = promise => {
-	let wrapper = new Promise(resolve => {
-		promise.then(
-			result => resolve([false, result]),
-			error => resolve([true, error])
-		);
-	});
+  let wrapper = new Promise(resolve => {
+    Promise.resolve(promise).then(
+      result => resolve([false, result]),
+      error => resolve([true, error])
+    );
+  });
 
-	return () => new Promise((resolve, reject) => {
-		wrapper.then(([isError, value]) => {
-			if (isError) reject(value);
-			else resolve(value);
-		});
-	});
+  return () =>
+    new Promise((resolve, reject) => {
+      wrapper.then(([isError, value]) => {
+        if (isError) reject(value);
+        else resolve(value);
+      });
+    });
 };
